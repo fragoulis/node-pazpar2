f391af917ed6c69377cf0ce82830a043df116d89
John Fragkoulis <john.fragkoulis@gmail.com>
2014-08-28 17:50:56 +0300

    Implemented search(), show(), stat(). Updated tests.

--- index.js
+++ index.js
@@ -6,17 +6,36 @@ var http = require('http')
   ,xml = require('xml2js')
 ;
 
-var Pazpar2 = function() {
-
-  this.requestOptions = {
+/**
+ * Application's default settings
+ * @type {Object}
+ */
+var defaultSettings = {
+  keepAlive: 50000,
+  request: {
     hostname: 'localhost',
     port: '9004',
     path: '/search.pz2',
     method: 'GET'
-  };
+  },
+  show: {
+    sort: 'relevance',
+    block: 1,
+    num: 10
+  }
+}
 
-  this.session = null;
-  this.keepAlive = 50000;
+/**
+ * Constructor
+ */
+var Pazpar2 = function(options) {
+
+  options = options || {};
+
+  this.requestOptions = _.clone(defaultSettings.request);
+  this.session = options.session || null;
+  this.keepAlive = defaultSettings.keepAlive;
+  this.terms = options.terms || [];
   this.createdAt = null;
   this.updatedAt = null;
 }
@@ -24,43 +43,25 @@ var Pazpar2 = function() {
 /**
  * [get description]
  * @param  {[type]} query         [description]
- * @param  {[type]} errorCallback [description]
- * @param  {[type]} dataCallback  [description]
  * @return {[type]}               [description]
  */
-Pazpar2.prototype.get = function(query, dataCallback) {
+var get = function(query) {
   var options = _.clone(this.requestOptions);
   options.path += '?' + querystring.stringify(query);
-  return http.get(options, function(response) {
-    response.on('data', dataCallback);
-  });
-};
 
-/**
- * Initialiazes a new session with the pz2 server.
- * 
- * @param  {array} options [description]
- * @return {Promise}       [description]
- */
-Pazpar2.prototype.init = function() {
   var self = this;
   return q.Promise(function(resolve, reject) {
-    
-    var query = {
-      command: 'init'
-    };
 
-    self.get(query, function (data) {
-      self.createdAt = new Date().toUTCString();
-
-      xml.parseString(data, function(err, result) {
-        self.session = result.init.session;
-        self.keepAlive = result.init.keepAlive;
+    http.get(options, function(response) {
+      response.on('data', function(data) {
+        xml.parseString(data, function(err, result) {
+          if (result.error) {
+            reject(new Error(util.format('Pazpar2 (%s): %s [%s]', result.error.$.code, result.error.$.msg, querystring.stringify(query))));
+          } else {
+            resolve(result);
+          }
+        });
       });
-
-      console.log('Pazpar2 session initialized [%s] @ %s', self.session, self.createdAt);
-
-      resolve(self);
     }).on('error', function(e) {
       reject(new Error(util.format('Pazpar2: %s', e.message)));
     });
@@ -68,6 +69,46 @@ Pazpar2.prototype.init = function() {
   });
 }
 
+/**
+ * [isInitialized description]
+ * @return {Boolean} [description]
+ */
+var isInitialized = function() {
+  return (this.session !== null);
+}
+
+/**
+ * Initialiazes a new session with the pz2 server.
+ * 
+ * @param  {array} options [description]
+ * @return {Promise}       [description]
+ */
+Pazpar2.prototype.init = function(params) {
+
+  // params = {
+  //   session: '',
+  //   keepAlive: '',
+  // }
+  
+  var self = this;
+  return q.Promise(function(resolve, reject) {
+    
+    var params = {
+      command: 'init'
+    };
+
+    return get.call(self, params)
+      .then(function(result) {
+        self.createdAt = new Date().toUTCString();
+        self.session = result.init.session;
+        self.keepAlive = result.init.keepAlive;
+
+        console.log('Pazpar2 session initialized [%s] @ %s', self.session, self.createdAt);
+
+        resolve(result);
+      }, reject);
+  });
+}
 
 /**
  * Pings the pz2 to keep the session alive.
@@ -78,25 +119,221 @@ Pazpar2.prototype.ping = function() {
   var self = this;
   return q.Promise(function(resolve, reject) {
     
-    var query = {
+    var params = {
       command: 'ping',
       session: self.session
     };
 
-    self.get(query, function(data) {
-      self.updatedAt = new Date().toUTCString();
-      xml.parseString(data, function(err, result) {
-        if (result.error) {
-          reject(new Error(util.format('Pazpar2 (%s): %s [%s]', result.error.$.code, result.error.$.msg, querystring.stringify(query))));
-        } else {
-          resolve(self);
-        }
-      });
-    }).on('error', function(e) {
-      reject(new Error(util.format('Pazpar2: %s', e.message)));
-    });
+    return get.call(self, params)
+      .then(function(result) {
+        self.updatedAt = new Date().toUTCString();
 
+        resolve(result);
+      }, reject);
   });  
 }
+
+/**
+ * [search description]
+ * @param  {array|string} options [description]
+ * @return {[type]}        [description]
+ */
+Pazpar2.prototype.search = function(options) {
+  var self = this;
+
+  // options = {
+  //   query: '',
+  //   filter: '',
+  //   onStat: function() {},
+  //   onShow: function() {},
+  //   onTerms: function() {}
+  // }
+
+  if (typeof options === 'string') {
+    options = {
+      query: options
+    };
+  }
+
+  if (options.query === undefined || options.query === '') {
+    throw new Error('Search query cannot must be defined and not empty.');
+  }
+
+  return q.Promise(function(resolve, reject) {
+
+    var params = {
+      command: 'search',
+      session: self.session,
+      query: options.query
+    };
+
+    if (options.filter) {
+      params.filter = filter;
+    }
+
+    return get.call(self, params)
+      .then(function(result) {
+        resolve(result);
+      }, reject);
+  });
+}
+
+/**
+ * [createStatObject description]
+ * @param  {[type]} result [description]
+ * @return {[type]}        [description]
+ */
+var StatObject = function(result) {
+  
+  // var stat = {};
+  // Object.keys(result.stat).forEach(function(key) {
+  //   var val = result.stat[key][0];
+  //   stat[key] = val;
+  // });
+  
+  return {
+    activeclients: parseInt(result.activeclients[0]),
+    hits: parseInt(result.hits[0]),
+    records: parseInt(result.records[0]),
+    clients: parseInt(result.clients[0]),
+    unconnected: parseInt(result.unconnected[0]),
+    connecting: parseInt(result.connecting[0]),
+    working: parseInt(result.working[0]),
+    idle: parseInt(result.idle[0]),
+    failed: parseInt(result.failed[0]),
+    error: parseInt(result.error[0]),
+    progress: parseFloat(result.progress[0])
+  };
+}
+
+/**
+ * [stat description]
+ * @return {[type]} [description]
+ */
+Pazpar2.prototype.stat = function() {
+  var self = this;
+  return q.Promise(function(resolve, reject) {
+    
+    var params = {
+      command: 'stat',
+      session: self.session
+    };
+
+    return get.call(self, params)
+      .then(function(result) {
+        var stat = new StatObject(result.stat);
+        resolve(stat);
+      }, reject);
+  });
+}
+
+/**
+ * [ShowObject description]
+ * @param {[type]} result [description]
+ */
+var ShowObject = function(result) {
+
+  var show = {};
+
+  show.status = result.status[0];
+  show.activeclients = parseInt(result.activeclients[0]);
+  show.merged = parseInt(result.merged[0]);
+  show.total = parseInt(result.total[0]);
+  show.start = parseInt(result.start[0]);
+  show.num = parseInt(result.num[0]);
+  show.hit = [];
+
+  for(var idxHit in result.hit) {
+    var newHit = {}
+      , oldHit = result.hit[idxHit];
+
+    newHit.title = oldHit['md-title'][0];
+    newHit.author = oldHit['md-author_070'];
+    newHit.publisher = oldHit['md-author_650'];
+    newHit.year = oldHit['md-year'][0];
+    newHit.count = parseInt(oldHit.count[0]);
+    newHit.relevance = parseInt(oldHit.relevance[0]);
+    newHit.recid = oldHit.recid[0];
+    newHit.recno = parseInt(oldHit.location[0]['md-recno'][0]);
+    newHit.holdings = [];
+
+    for(var idxLoc in oldHit.location) {
+      var holding = {}
+        , location = oldHit.location[idxLoc];
+
+      holding.source = location.$.id;
+      holding.checksum = location.$.checksum;
+      holding.digital = location['md-digital'];
+
+      newHit.holdings.push(holding);
+    }
+
+    show.hit.push(newHit);
+  }
+
+  return show;
+}
+
+/**
+ * [show description]
+ * @return {[type]} [description]
+ */
+Pazpar2.prototype.show = function(options) {
+  var self = this
+    , options = options || {}
+    ;
+
+  // options = {
+  //   start: 0,
+  //   num: 0,
+  //   block: 1,
+  //   sort: 'relevance',
+  //   mergekey: '',
+  //   rank: ''
+  // };
+
+  return q.Promise(function(resolve, reject) {
+
+    var params = {
+      command: 'show',
+      session: self.session,
+      block: options.block || defaultSettings.show.block,
+      num: options.num || defaultSettings.show.num,
+      sort: options.sort || defaultSettings.show.sort,
+      start: options.start || 0,
+      type: 'xml'
+    };
+
+    return get.call(self, params)
+      .then(function(result) {
+        var show = new ShowObject(result.show);
+        resolve(show);
+      }, reject);
+  });
+}
+
+/**
+ * [termlist description]
+ * @return {[type]} [description]
+ */
+Pazpar2.prototype.termlist = function() {
+  var self = this;
+
+  return q.Promise(function(resolve, reject) {
+
+    var params = {
+      command: 'termlist',
+      session: self.session,
+      name: self.terms.join(',')
+    }
+
+    return get.call(self, params)
+      .then(function(result) {
+        resolve(result);
+      }, reject);
+
+  });
+}
+
 
 module.exports = Pazpar2;
--- test/index.js
+++ test/index.js
@@ -3,7 +3,10 @@ var assert = require('assert');
 
 describe('Parpar2', function() {
 
-  var pz2 = new Pazpar2();
+  var pz2 = new Pazpar2({
+    session: '2130136065',
+    terms: ['subject']
+  });
 
   it('initializes the connection', function(done) {
     pz2.init().then(function(o) {
@@ -20,5 +23,39 @@ describe('Parpar2', function() {
       done(err);
     });
   });
+
+  it('searches for "ti=saint john"', function(done) {
+    pz2.search("ti=saint john").then(function(o) {
+      setTimeout(done, 1800);
+    }, function(err) {
+      done(err);
+    });
+  });
+
+  it('stats for last search', function(done) {
+    pz2.stat().then(function(o) {
+      done();
+    }, function(err) {
+      done(err);
+    });
+  });
+
+  it('shows top records', function(done) {
+    pz2.show().then(function(o) {
+      done();
+    }, function(err) {
+      done(err);
+    });
+  });
+
+  // it('shows termlist', function(done) {
+  //   pz2.termlist().then(function(o) {
+  //     // console.log(o.termlist.list[0].$);
+  //     // console.log(o.termlist.list[0].term);
+  //     done();
+  //   }, function(err) {
+  //     done(err);
+  //   });
+  // });
 
 });
